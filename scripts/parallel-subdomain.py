d31c827f31b244cab896f5252d0a70b3e2408d35
brunowollinger <bruno.wollinger@outlook.com>
2023-08-10 01:27:54 -0300

    Added conditions to prevent error during access of inexistent files and removed the Sublist3r application from the execution

--- scripts/parallel-subdomain.py
+++ scripts/parallel-subdomain.py
@@ -5,11 +5,13 @@ target = sys.argv[1]
 domain = sys.argv[2]
 
 def parallel():
-    os.system(f'rm -rf /docker/data/{target}/tmp/subdomain_parallel.log')
-    with open (f'/docker/data/{target}/tmp/subdomain_parallel.log','a') as file:
+    if os.path.isfile(f'/docker/data/{target}/tmp/subdomain_parallel.log'):
+        os.system(f'rm -f /docker/data/{target}/tmp/subdomain_parallel.log')
+    os.system(f'touch /docker/data/{target}/tmp/subdomain_parallel.log')
+    with open (f'/docker/data/{target}/tmp/subdomain_parallel.log','w') as file:
         file.write(f'python3 /docker/scripts/automation-assetfinder.py {target} {domain}\n')
         file.write(f'python3 /docker/scripts/automation-subfinder.py {target} {domain}\n')
-        file.write(f'python3 /docker/scripts/automation-sublist3r.py {target} {domain}\n')
+        # file.write(f'python3 /docker/scripts/automation-sublist3r.py {target} {domain}\n')
     print("\033[34m[+] PROCESSANDO SUBDOMAIN\n\033[0m")
     os.system(f'cat /docker/data/{target}/tmp/subdomain_parallel.log | parallel -u')
 
