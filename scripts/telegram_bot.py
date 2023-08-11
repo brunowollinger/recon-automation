334d0d848cbaacb914df3aff2ef0219a39890194
brunowollinger <bruno.wollinger@outlook.com>
2023-08-11 01:30:38 -0300

    Removed useless verbose during message submition

--- scripts/telegram_bot.py
+++ scripts/telegram_bot.py
@@ -1,13 +1,11 @@
 import requests
 import json
 
-def sendMessage():
+def sendMessage(message):
     with open('/docker/scripts/telegram-credentials.json', 'r') as file:
         credentials = json.load(file)
     apiurl = f'https://api.telegram.org/bot{credentials["token"]}/sendMessage'
-    response = requests.post(apiurl, json={'chat_id': f'{credentials["chat_id"]}', 'text': 'Hello Bot, again!'})
-    if response.status_code == 200:
-        print("\033[32m[v] Mensagem enviada.\n\033[0m")
+    requests.post(apiurl, json={'chat_id': f'{credentials["chat_id"]}', 'text': f'{message}'})
 
 if __name__ == '__main__':
-    sendMessage()
+    sendMessage('Debug Message')
