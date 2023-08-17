6bac56ac57620a3d653923691416f7e654ac22b1
brunowollinger <bruno.wollinger@outlook.com>
2023-08-17 19:53:58 -0300

    Change auth file to secrets folder

--- scripts/telegramBot.py
+++ scripts/telegramBot.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def sendMessage(message):
-    with open('/docker/scripts/telegramCredentials.json', 'r') as file:
+    with open('/docker/secrets/telegramCredentials.json', 'r') as file:
         credentials = json.load(file)
     apiurl = f'https://api.telegram.org/bot{credentials["token"]}/sendMessage'
     requests.post(apiurl, json={'chat_id': f'{credentials["chat_id"]}', 'text': f'{message}'})
